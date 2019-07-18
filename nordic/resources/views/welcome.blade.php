8bcb49babbeb02d8275ad2c90e91b556ed98d8be
Owoeye Femi <famofash@gmail.com>
2019-07-18 13:43:39 -0400

    initial setup

---
+++ nordic/resources/views/welcome.blade.php
@@ -0,0 +1,443 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js"> <!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<title>The Nordic hospitality Suites- Jabi, Abuja</title>
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+<link rel="shortcut icon" img="/favicon.ico" />
+<!-- CSS FILES -->
+<link rel="stylesheet" href="{{asset('resources/assets/css/bootstrap.min.css')}} ">
+<link rel="stylesheet" href="{{asset('resources/assets/css/supersized.css')}}">
+<link rel="stylesheet" href="{{asset('resources/assets/css/supersized.shutter.css')}}">
+<link rel="stylesheet" href="{{asset('resources/assets/css/flexslider.css')}}">
+<link rel="stylesheet" href="{{asset('resources/assets/css/prettyPhoto.css')}}">
+<link rel="stylesheet" href="{{asset('resources/assets/css/datepicker.css')}}">
+<link rel="stylesheet" href="{{asset('resources/assets/css/selectordie.css')}}">
+<link rel="stylesheet" href="{{asset('resources/assets/css/main.css')}}">
+<link rel="stylesheet" href="{{asset('resources/assets/css/2035.responsive.css')}}">
+
+<script src="resources/assets/js/vendor/modernizr-2.8.3-respond-1.1.0.min.js"></script>
+<!-- Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+  <script src="resources/assets/js/respond.min.js"></script>
+<![endif]-->
+</head>
+<body>
+<!--[if lt IE 7]>
+    <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+<![endif]-->
+<div id="wrapper">
+	<div id="home">
+		<div class="header"><!-- Header Section -->
+			<div class="pre-header"><!-- Pre-header -->
+				<div class="container">
+					<div class="row">
+						<div class="pull-left pre-address-b"><p><i class="fa fa-map-marker"></i> 
+                                                        No. 14, Patrick O. Bokkor Crescent
+Jabi, Abuja</p></div>
+						<div class="pull-right">
+							<div class="pull-left">
+								<ul class="pre-link-box">
+									<li><a href="#">About</a></li>
+									<li><a href="#">Contact</a></li>
+                                                                        <li><a href="#">Terms & Conditions</a></li>
+									
+								</ul>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="main-header"><!-- Main-header -->
+				<div class="container">
+					<div class="row">
+						<div class="pull-left">
+							<div class="logo">
+                                                            <a href="#"><img alt="Logo" src="resources/assets/img/logo-grey.png" class="img-responsive" style="width: 120px; height: auto" /></a>
+							</div>
+						</div>
+						<div class="pull-right">
+							<div class="pull-left">
+								<nav class="nav">
+									<ul id="navigate" class="sf-menu navigate">
+										<li class="active"><a href="index.html">HOME</a>											
+										</li>
+										<li class="parent-menu"><a href="#">OUR ROOMS</a>
+											<ul>
+												<li></li>
+												
+																								
+											</ul>
+										</li>
+										
+										<li><a href="#">GALLERY</a></li>
+										<li><a href="#">CONTACT</a></li>
+									</ul>
+								</nav>
+							</div>
+							<div class="pull-right">
+								<div class="button-style-1 margint45">
+									<a href="reservation-form-dark.html"><i class="fa fa-calendar"></i>BOOK NOW</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="book-slider">
+			<div class="container">
+				<div class="row pos-center">
+					<div class="reserve-form-area">
+						<form action="#" method="post" id="ajax-reservation-form">
+							<ul class="clearfix">
+								<li class="li-input">
+									<label>ARRIVAL</label>
+									<input type="text" id="dpd1" name="dpd1" class="date-selector" placeholder="&#xf073;" />
+								</li>
+								<li class="li-input">
+									<label>DEPARTURE</label>
+									<input type="text" id="dpd2" name="dpd2" class="date-selector" placeholder="&#xf073;" />
+								</li>
+								<li class="li-select">
+									<label>ROOMS</label>
+									<select name="rooms" class="pretty-select">
+										<option selected="selected" value="1" >1</option>
+										<option value="2">2</option>
+										<option value="3">3</option>
+										<option value="4">4</option>
+										<option value="5">5</option>
+									</select>
+								</li>
+								<li class="li-select">
+									<label>ADULT</label>
+									<select name="adult" class="pretty-select">
+										<option selected="selected" value="1" >1</option>
+										<option value="2">2</option>
+										<option value="3">3</option>
+										<option value="4">4</option>
+										<option value="5">5</option>
+									</select>
+								</li>
+								<li class="li-select">
+									<label>CHILDREN</label>
+									<select name="children" class="pretty-select">
+										<option selected="selected" value="0" >0</option>
+										<option value="1">1</option>
+										<option value="2">2</option>
+										<option value="3">3</option>
+										<option value="4">4</option>
+										<option value="5">5</option>
+									</select>
+								</li>
+								<li>
+									<div class="button-style-1 margint40">
+										<a id="res-submit" href="#"><i class="fa fa-search"></i>SEARCH</a>
+									</div>
+								</li>
+							</ul>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="bottom-book-slider">
+			<div class="container">
+				<div class="row pos-center">
+					<ul>
+						<li><i class="fa fa-phone"></i> 24/7HRS CUSTOMER SERVICE</li>
+						<li><i class="fa fa-car"></i> AIRPORT PICK UP</li>
+						<li><i class="fa fa-coffee"></i> COFFEE & BREAKFAST FREE</li>
+						<li><i class="fa fa-wifi"></i> FREE WI-FI ALL ROOM</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="content"><!-- Content Section -->
+		<div class="about clearfix"><!-- About Section -->
+			<div class="container">
+				<div class="row">
+					<div class="about-title pos-center margint60">
+						<h2>WELCOME TO NORDIC HOSPITALITY SUITES</h2>
+						<div class="title-shape"><img alt="Shape" src="resources/assets/img/shape.png"></div>
+						<p>Welcome to our hotel, Nordic Hospitality is a boutique hotel located in Jabi.
+                                                    We offer 22 unique state of the art rooms furnished in a modern day
+                                                    style and decorated with designer furniture.
+                                                    Our rooms are ideal for travelers and families who want the best of leisure time. </p>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+		<div class="explore-rooms margint30 clearfix"><!-- Explore Rooms Section -->
+			<div class="container">
+				<div class="row">	
+					<div class="title-style-2 marginb40 pos-center">
+						<h3>EXPLORE ROOMS</h3>
+						<hr>
+					</div>
+					<div class="col-lg-4 col-sm-6">
+						<div class="home-room-box">
+							<div class="room-image">
+								<img alt="Room Images" class="img-responsive" src="resources/assets/img/rooms/room-2.JPG">
+								<div class="home-room-details">
+									<h5><a href="#">The Standard Room</a></h5>
+									<div class="pull-left">
+										<ul>
+											<li><i class="fa fa-calendar"></i></li>
+											<li><i class="fa fa-flask"></i></li>
+											<li><i class="fa fa-umbrella"></i></li>
+											<li><i class="fa fa-laptop"></i></li>
+										</ul>
+									</div>
+									<div class="pull-right room-rating">
+										<ul>
+											<li><i class="fa fa-star"></i></li>
+											<li><i class="fa fa-star"></i></li>
+											<li><i class="fa fa-star"></i></li>
+											<li><i class="fa fa-star"></i></li>
+											<li><i class="fa fa-star inactive"></i></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<div class="room-details">
+								<p>Our standards Rooms are comfortably furnished with a queen sized bed, a writing desk, 
+                                                                    32" flat-screen TV, High Speed Internet. A Standard Room can accommodate one guest[...]</p>
+							</div>
+							<div class="room-bottom">
+								<div class="pull-left"><h4>89$<span class="room-bottom-time">/ Day</span></h4></div>
+								<div class="pull-right">
+									<div class="button-style-1">
+										<a href="#">BOOK NOW</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-sm-6">
+						<div class="home-room-box">
+							<div class="room-image">
+								<img alt="Room Images" class="img-responsive" src="resources/assets/img/rooms/room-04.JPG">
+								<div class="home-room-details">
+									<h5><a href="#">The Nordic Suite</a></h5>
+									<div class="pull-left">
+										<ul>
+											<li><i class="fa fa-calendar"></i></li>
+											<li><i class="fa fa-flask"></i></li>
+											<li><i class="fa fa-umbrella"></i></li>
+											<li><i class="fa fa-laptop"></i></li>
+										</ul>
+									</div>
+									<div class="pull-right room-rating">
+										<ul>
+											<li><i class="fa fa-star"></i></li>
+											<li><i class="fa fa-star"></i></li>
+											<li><i class="fa fa-star"></i></li>
+											<li><i class="fa fa-star"></i></li>
+											<li><i class="fa fa-star inactive"></i></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<div class="room-details">
+								<p>
+                                                                    Our Nordic Suites are spaciously sized with a king size bed, a large sitting area, a writing desk. 
+                                                                    The Suite is furnished to suit your need, its also perfect as an office while staying with us.
+                                                                  [...]</p>
+							</div>
+							<div class="room-bottom">
+								<div class="pull-left"><h4>89$<span class="room-bottom-time">/ Day</span></h4></div>
+								<div class="pull-right">
+									<div class="button-style-1">
+										<a href="#">BOOK NOW</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-sm-6">
+						<div class="home-room-box">
+							<div class="room-image">
+								<div class="room-features">FEATURED</div>
+								<img alt="Room Images" class="img-responsive" src="resources/assets/img/rooms/room-5.JPG">
+								<div class="home-room-details">
+									<h5><a href="#">The Deluxe
+                                                                            </a></h5>
+									<div class="pull-left">
+										<ul>
+											<li><i class="fa fa-calendar"></i></li>
+											<li><i class="fa fa-flask"></i></li>
+											<li><i class="fa fa-umbrella"></i></li>
+											<li><i class="fa fa-laptop"></i></li>
+										</ul>
+									</div>
+									<div class="pull-right room-rating">
+										<ul>
+											<li><i class="fa fa-star"></i></li>
+											<li><i class="fa fa-star"></i></li>
+											<li><i class="fa fa-star"></i></li>
+											<li><i class="fa fa-star"></i></li>
+											<li><i class="fa fa-star inactive"></i></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<div class="room-details">
+								<p>Spaciously sized with a king size bed. A large sitting area, a writing desk,
+                                                                    a Full Size Bathtub with luxurious bath products and the scented candles. 
+                                                                    This room is perfect not only for business but also for a romantic getaway[...]</p>
+							</div>
+							<div class="room-bottom">
+								<div class="pull-left"><h4>89$<span class="room-bottom-time">/ Day</span></h4></div>
+								<div class="pull-right">
+									<div class="button-style-1">
+										<a href="#">BOOK NOW</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	
+		<div class="newsletter-section"><!-- Newsletter Section -->
+			<div class="container">
+				<div class="row">
+					<div class="newsletter-top pos-center margint30">
+						<img alt="Shape Image" src="resources/assets/img/shape.png" >
+					</div>
+					<div class="newsletter-form margint40 pos-center">
+						<div class="newsletter-wrapper">
+							<div class="pull-left">
+								<h2>Sign up newsletter</h2>
+							</div>
+							<div class="pull-left">
+								<form action="#" method="post" id="ajax-contact-form">
+									<input type="text" placeholder="Enter a e-mail address">
+									<input type="submit" value="SUBSCRIBE" >
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="footer margint40"><!-- Footer Section -->
+			<div class="main-footer">
+				<div class="container">
+					<div class="row">
+						
+						<div class="col-lg-10 col-sm-10">
+							<div class="col-lg-3 col-sm-3">
+								<h6>TOUCH WITH US</h6>
+								<ul class="footer-links">
+									<li><a href="#">Facebook</a></li>
+									<li><a href="#">Twitter</a></li>
+									<li><a href="#">Google +</a></li>
+									<li><a href="#">otels.com</a></li>
+									<li><a href="#">Tripadvisor</a></li>
+								</ul>
+							</div>
+							<div class="col-lg-3 col-sm-3">
+								<h6>NORDIC HOSPITALITY SUITE</h6>
+								<ul class="footer-links">
+									
+									<li><a href="#">About</a></li>
+									<li><a href="#">Blog</a></li>
+									
+								</ul>
+							</div>
+							<div class="col-lg-3 col-sm-3">
+								<h6>LINKS</h6>
+								<ul class="footer-links">
+									<li><a href="#">Contact</a></li>
+									<li><a href="#">Gallery</a></li>
+									
+								</ul>
+							</div>
+							<div class="col-lg-3 col-sm-3">
+								<h6>CONTACT</h6>
+								<ul class="footer-links">
+									<li><p><i class="fa fa-map-marker"></i> No. 14, Patrick O. Bokkor Crescent Jabi, Abuja </p></li>
+									<li><p><i class="fa fa-phone"></i> For Reservation: +2347080266555 </p></li>
+									<li><p><i class="fa fa-envelope"></i> <a href="mailto:info@nordichospitalitysuites.com">info@nordichospitalitysuites.com</a></p></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="pre-footer">
+				<div class="container">
+					<div class="row">
+						<div class="pull-left"><p>© LUXEN OTELS 2014</p></div>
+						<div class="pull-right">
+							<ul>
+								<li><p>CONNECT WITH US</p></li>
+								<li><a><img alt="Facebook" src="resources/assets/temp/orkut.png" ></a></li>
+								<li><a><img alt="Tripadvisor" src="resources/assets/temp/tripadvisor.png" ></a></li>
+								<li><a><img alt="Yelp" src="resources/assets/temp/hyves.png" ></a></li>
+								<li><a><img alt="Twitter" src="resources/assets/temp/skype.png" ></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- JS FILES -->
+<script src="resources/assets/js/vendor/jquery-1.11.1.min.js"></script>
+<script src="resources/assets/js/vendor/bootstrap.min.js"></script>
+<script src="resources/assets/js/retina-1.1.0.min.js"></script>
+<script src="resources/assets/js/jquery.flexslider-min.js"></script>
+<script src="resources/assets/js/superfish.pack.1.4.1.js"></script>
+<script src="resources/assets/js/jquery.slicknav.min.js"></script>
+<script src="resources/assets/js/bootstrap-datepicker.js"></script>
+<script src="resources/assets/js/selectordie.min.js"></script>
+<script src="resources/assets/js/jquery.parallax-1.1.3.js"></script>
+<script src="resources/assets/js/jquery.prettyPhoto.js"></script>
+<script src="resources/assets/js/supersized.3.2.7.min.js"></script>
+<script src="resources/assets/js/supersized.shutter.min.js"></script>
+<script src="resources/assets/js/main.js"></script>
+<script type="text/javascript">
+	jQuery(function($){				
+	$.supersized({
+		slideshow			:1,			// Slideshow on/off
+		autoplay			:1,			// Slideshow starts playing automatically
+		slide_interval		:4000,		// Length between transitions
+		transition			:1, 			// 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
+		transition_speed	:1000,		// Speed of transition
+		pause_hover			:0,			// Pause slideshow on hover
+		performance			:1,			// 0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
+		image_protect		:1,			// Disables image dragging and right click with Javascript
+		slides				:[			// Slideshow Images
+                                                                {image : 'resources/assets/temp/e.jpg'},
+								{image : 'resources/assets/temp/f.jpg'},
+                                                                {image : 'resources/assets/temp/b.jpg'},
+								{image : 'resources/assets/temp/b.jpg'},
+								{image : 'resources/assets/temp/c.jpg'}
+							 ]
+		});
+	});	  
+</script>
+<!--
+<script>
+    var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src='//www.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g,s)}(document,'script'));
+</script>
+-->
+</body>
+</html>
